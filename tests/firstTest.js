b0ede7dbfc6f706c87a8f09790079eb029b65635
HernanAlonso <hernanmalonso@gmail.com>
2023-08-07 23:33:49 -0300

    Selenium Demo for google
    Test for Google to search Selenium and pick the second visible result

---
+++ tests/firstTest.js
@@ -0,0 +1,48 @@
+const { Builder, By, Key, until, Capabilities } = require("selenium-webdriver");
+const assert = require("assert");
+var should = require("chai").should();
+
+describe("Test for Google search keyword Selenium ", () => {
+  it("Open second result in new tab and check value of tab", async () => {
+    let driver = await new Builder()
+      .withCapabilities(Capabilities.firefox())
+      .build();
+    let baseURL = "https://www.google.com";
+    try {
+      driver.get(baseURL);
+
+      const searchInput = await driver.findElement(By.id("APjFqb"));
+      await searchInput.sendKeys("selenium", Key.RETURN);
+
+      await driver.wait(
+        until.elementsLocated(By.css("div[lang][data-ved] a[data-ved]")),
+        5000
+      );
+
+      const results = await driver.findElements(By.css("div[lang][data-ved]"));
+      for (let i = 1; i < results.length; i++) {
+        const div = results[i];
+        if (await div.isDisplayed()) {
+          const link = await div.findElement(By.css("a[data-ved]"));
+          await driver.executeScript("arguments[0].scrollIntoView();", link);
+          await driver
+            .actions()
+            .keyDown(Key.COMMAND)
+            .click(link)
+            .keyUp(Key.COMMAND)
+            .perform();
+          const tabs = await driver.getAllWindowHandles();
+          await driver.switchTo().window(tabs[1]);
+          await driver.wait(until.elementLocated(By.css("h1")), 10000);
+          const newTabTitle = await driver.getTitle();
+          console.log(newTabTitle);
+          break;
+        }
+      }
+    } catch (err) {
+      console.error("Error occured: ", err);
+    } finally {
+      await driver.quit();
+    }
+  });
+});
